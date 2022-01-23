3ebb5f53a61ad3763355d9806077e833728e3f5e
jwmenjivar <jwmenjivar@gmail.com>
2022-01-23 15:56:59 -0600

    added sysouts to see how it works

--- hrrank-date/src/com/company/Main.java
+++ hrrank-date/src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
+    public static String findDay(int month, int day, int year) {
+        String weekDay = null;
+        try {
+            String date = day + "/" + month + "/" + year;
+            System.out.println(date);
+            Date date1 = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+            System.out.println(date1);
+
+            SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE");
+            System.out.println(simpleDateformat);
+
+            weekDay=simpleDateformat.format(date1).toUpperCase( );
+            System.out.println(weekDay);
+
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return weekDay;
+    }
+
+
+
     public static void main(String[] args) {
-	// write your code here
+        findDay(2,25,1989);
     }
 }
