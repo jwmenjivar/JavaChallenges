ee781f80447ed8065fb42e78a2c379fcef556924
jwmenjivar <jwmenjivar@gmail.com>
2022-01-23 16:25:03 -0600

    added notes about parsing and simpledateformat

--- hrrank-date/src/com/company/Main.java
+++ hrrank-date/src/com/company/Main.java
@@ -9,8 +9,19 @@ public class Main {
         String weekDay = null;
         try {
             String date = day + "/" + month + "/" + year;
+            /*
+            We define the order of month day year in the line above
+            (We could have said year month day or year day month... but...
+            we would have to change the pattern in line 18, to match that,
+            otherwise it won't work)
+            */
             System.out.println(date);
             Date date1 = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+            /*
+            .parse() converts the String date into a Date data type(From String to Date Object).
+            SimpleDateFormat matches the pattern in its argument with what was defined inside parse().
+            And all that is stored in date1
+            */
             System.out.println(date1);
 
             SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE");
