39d9feb9c77362a6ab7045c50600264d70c3344a
jwmenjivar <jwmenjivar@gmail.com>
2022-02-07 22:42:48 -0600

    adding method to controller with autowired

---
+++ Springboot/App1/src/main/java/com/appsaprendiento/App1/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.appsaprendiento.App1.repository;
+
+import com.appsaprendiento.App1.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
