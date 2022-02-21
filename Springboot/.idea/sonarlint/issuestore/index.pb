2af8e67379f496e61683af74d7272978d590f72d
jwmenjivar <jwmenjivar@gmail.com>
2022-02-21 04:25:04 -0800

    added matrix diagonal sum

---
+++ Springboot/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+p
+@App1/src/main/java/com/appsaprendiento/App1/App1Application.java,9/5/95feb896439c22ac0d55f25f9392207c05f92d8b
+z
+JApp1/src/main/java/com/appsaprendiento/App1/service/DepartmentService.java,c/2/c2f6bf290843d6eec041ac012d544b0d1adb58b8
+€
+PApp1/src/main/java/com/appsaprendiento/App1/controller/DepartmentController.java,b/f/bf96928b3a3142b5b493c69b6a03c1cc97066200
+r
+BApp1/src/main/java/com/appsaprendiento/App1/entity/Department.java,4/3/43091cca961c058a922c3a1c24153443f4033f94
+~
+NApp1/src/main/java/com/appsaprendiento/App1/service/DepartmentServiceImpl.java,9/1/91696ff0b64a5b23a1e348e879c5705478902f9e
+€
+PApp1/src/main/java/com/appsaprendiento/App1/repository/DepartmentRepository.java,2/b/2b6975effcc1bbcbac5658fc839f32c8cfaf1c72
--- hrrank-matrixDiagonalSum/src/com/company/Main.java
+++ hrrank-matrixDiagonalSum/src/com/company/Main.java
@@ -1,8 +1,69 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        //
+        System.out.println("From inside the diagonal method" + arr);
+
+        //So we want to first addition we want to get 0 0 of the first list
+        //the get 1 1 of the second list
+        // and the 2 2 of the third
+        int sum1 =0;
+        for(int i = 0 ; i<arr.size() ; i++){
+            sum1 += arr.get(i).get(i);
+        }
+        System.out.println("This is sum1 = "+ sum1);
+        //for the second addition, 0 2//1 1// 2 0
+        int sum2 =0;
+        int decrement = 0;
+        for(int i = 0 ; i<arr.size() ; i++){
+
+            //row puede ir en aumento
+            for (int j = arr.get(i).size()-1 ; j>=0 ; j--){
+                //column primero es the length of row
+                //la segunda es length of row -1
+                //la segunda es length of row -1 -1
+                if(){}
+
+                sum2 += arr.get(i).get(j) - decrement;
+                System.out.println(sum2);
+                decrement++;
+
+            }
+        }
+        int diff = sum1 - sum2;
+        int abs = Math.abs(diff);
+        return abs;
+
+    }
+    
     public static void main(String[] args) {
 	// write your code here
+
+        List<List<Integer>> numbers = new ArrayList<>();
+
+        List<Integer> firstRow = new ArrayList<Integer>();
+        firstRow.add(3); firstRow.add(2); firstRow.add(53);
+        System.out.println(firstRow);
+
+        List<Integer> secondRow = new ArrayList<Integer>();
+        secondRow.add(3); secondRow.add(6); secondRow.add(5);
+        System.out.println(secondRow);
+
+        List<Integer> thirdRow = new ArrayList<Integer>();
+        thirdRow.add(6); thirdRow.add(7); thirdRow.add(1);
+        System.out.println(thirdRow);
+
+        numbers.add(firstRow);
+        numbers.add(secondRow);
+        numbers.add(thirdRow);
+
+        System.out.println(numbers);
+
+        diagonalDifference(numbers);
     }
 }
