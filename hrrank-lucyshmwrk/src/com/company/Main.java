8ccb75193747c60707e9e1e3a5af09b0ae2cbb03
jwmenjivar <jwmenjivar@gmail.com>
2022-02-23 22:37:24 -0600

    added exercises

---
+++ hrrank-lucyshmwrk/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static int lilysHomework(List<Integer> arr) {
+
+
+
+//So the core thing is a swap. This is a loop that will contain a counter++ at the end that will be returned as the response to the whole exercise
+
+    //the swap is to find the smallest value
+        //we do this by looping through the array
+    for(int i=0 ; i<arr.size() ; i++){
+
+    }
+    //to find out if it has the lowest index
+    //if not then look for the item containing the lowest
+    //then using temporary variables, perform the swap.
+    // swap is completed now count++
+
+    public static void main(String[] args) {
+	// write your code here
+            List<Integer> arr1 = new ArrayList<>();
+            
+
+            lilysHomework(arr1);
+    }
+}
+}
