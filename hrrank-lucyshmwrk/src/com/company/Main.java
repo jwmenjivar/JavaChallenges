3587421e34812338acaddf2c97ea2aca0e196416
jwmenjivar <jwmenjivar@gmail.com>
2022-02-25 10:02:29 -0600

    been trying with lucy's

--- hrrank-lucyshmwrk/src/com/company/Main.java
+++ hrrank-lucyshmwrk/src/com/company/Main.java
@@ -1,39 +1,42 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static int lilysHomework(List<Integer> arr) {
-
-
+        int counter = 0;
 //So the core thing is a swap. This is a loop that will contain a counter++ at the end that will be returned as the response to the whole exercise
 
         //the swap is to find the smallest value
-        //we do this by looping through the array
-        for (int i = 0; i < arr.size(); i++) {
-            for(int j=0 ; j < arr.size() ; j++){
-                if(arr.get(i) < arr.get(j)){
-                    //we first remove to temporary
-                    int temporary = arr.get()
-                }
-            }
-        }
-        //to find out if it has the lowest index. We get its index
+        //we do this sorting the array
+        List<Integer> savedList = arr;
+        System.out.println("Saved List: "+savedList);
+        Collections.sort(arr);
+        System.out.println("Saved List: "+savedList);
+        int smallestNumber =arr.get(0);
 
+        //to find out if it has the lowest index. We get its index
+        int smallestNumberIndex = savedList.indexOf(45);
         //if not then look for the item containing the lowest
+        if(smallestNumberIndex != 0){
+            int tempNumber = savedList.get(0);
+            savedList.set(0,smallestNumber);
+            savedList.set(smallestNumberIndex,tempNumber);
+        counter++;
+        }
+        System.out.println(savedList);
         //then using temporary variables, perform the swap.
         // swap is completed now count++
-        return 1;
+        return counter;
     }
     public static void main (String[] args) {
 	// write your code here
             List<Integer> arr1 = new ArrayList<>(
-                    List.of(2,3,4,1)
+                    List.of(5,45,2,3,4,1)
             );
             System.out.println(arr1);
 
-            lilysHomework(arr1);
+            System.out.println("This is the number of swaps: "+lilysHomework(arr1));
     }
 }
 
