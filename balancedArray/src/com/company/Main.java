030be09deee909045ad7616a5b865478be94666a
jwmenjivar <jwmenjivar@gmail.com>
2022-03-14 17:55:28 -0700

    added info

--- balancedArray/src/com/company/Main.java
+++ balancedArray/src/com/company/Main.java
@@ -7,31 +7,29 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-String path[] = {"D","D","U","U","U","U","D","D","D","U","D","U"};
+        // write your code here
+        int a1 = 1; int a2 = 4;
+        int b1 = 2; int b2 = 5;
 
-        int seaLevel = 0;
-        int valleys = 0;
-        int inValley = 0;
+        int d1 = a1 - b2;
 
-        for(int i=0; i<path.length;i++){
+        int d2 = b1 - a2;
 
-            if(path[i] == "U"){
-                seaLevel++;
-            }
-            else {
-                seaLevel--;
-            }
-
-            if(seaLevel < 0){
-                inValley++;
-            }
-
-            if(seaLevel==0 && inValley>0){
-                valleys++;
-                inValley = 0;
-            }
-
+        Boolean intersect = false;
+        if( d1 >= 0 && d2 <= 0){
+            intersect = true;
         }
-        System.out.println(valleys);
-}
+
+//        if(intersect || (d1 >= 0 && d2<=0)){
+//            intersect = true;
+//        }
+        else{
+            intersect = false;
+        }
+
+        System.out.println("d1: "+d1);
+        System.out.println("d2: "+d2);
+        System.out.println("Do they intersect? " + intersect);
+
+    }
 }
